67527ce0d9863ce191c03459972fe2469f8dc654
Dionysusnu <dion.niessen@gmail.com>
2019-06-28 12:36:48 +0200

    Remove express website, replace with singlefile

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ client.on('error', error => {
 
 client.login(auth.token);
 
-const app = require('./Interface/app');
+const app = require('express')();
 
 app.post('/bot', (req, res) => {
 	console.log('got POST request on /bot:');
