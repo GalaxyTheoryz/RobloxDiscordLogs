15cac8d03ae00efe3f5ebde02d8d0c0bc041800d
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2019-07-20 13:41:40 +0200

    Unfinished update cleanup

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ client.on('ready', () => {
 
 const messagequeue = new Map();
 const commandqueue = new Map();
+const latestmessages = new Map();
 const channels = new Map();
 const isServerNumFree = function(num) {
 	return !messagequeue.has(num);
@@ -40,6 +41,7 @@ const addServer = async function(preferred) {
 	}
 	messagequeue.set(newservernum, []);
 	commandqueue.set(newservernum, []);
+	latestmessages.set(newservernum, new Date());
 	const channeldata = new Object();
 	channeldata.type = 'text';
 	channeldata.parent = categorychannel;
@@ -123,6 +125,23 @@ client.on('message', async message => {
 		for (const [, channel] of categorychannel.children) {
 			if (channel != fulllogchannel && !findServerFromChannel(channel)) {
 				channel.delete('Server shutdown');
+			}
+		}
+		for (const [server, lastdate] of latestmessages) {
+			if (lastdate < new Date() - 60) {
+						const channel = channels.get(bodydata.servernum);
+		const newserverembed = new RichEmbed().setTitle('New server').addField('Game:', bodydata.gamename).setFooter(new Date(channel.createdTimestamp)).setColor([0, 255, 0]);
+		fulllogchannel.send('', newserverembed);
+		for (const [, value] of channel.messages.filter(message => message.author.id == client.user.id)) {
+			// console.log(value.embeds);
+			// console.log(value)
+			await fulllogchannel.send('', new RichEmbed(value.embeds[0]).setColor([0, 0, 255]));
+		}
+		const servercloseembed = new RichEmbed().setTitle('Server shutdown').setFooter(new Date()).setColor([255, 0, 0]);
+		fulllogchannel.send('', servercloseembed);
+		channel.delete('Server shutdown');
+		channels.delete(bodydata.servernum);
+		messagequeue.delete(bodydata.servernum)
 			}
 		}
 		toedit.edit('Cleaned channels!');
